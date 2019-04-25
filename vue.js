2fe1ba89dd41161499194ccbe6b016479d96327a
christopheleduc <christophe.leduc@le-campus-numerique.fr>
2019-04-25 16:21:50 +0200

    02 decouverte V0.1

--- vue.js
+++ vue.js
@@ -35,3 +35,21 @@ new Vue({
       show: false,
     }
   })
+
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      seen: true
+    }
+  })
+
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      todos: [
+        { text: 'Apprendre JavaScript' },
+        { text: 'Apprendre Vue' },
+        { text: 'Créer quelque chose de génial' }
+      ]
+    }
+  })
